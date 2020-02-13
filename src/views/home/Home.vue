c5c1cfaba39001a4e80a1b2419908e9463e54dd1
Kugou <15727657758@163.com>
2020-02-13 22:28:56 +0800

    home page

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,42 @@
 <template>
-  <h2>首页</h2>
+  <div>
+    <nav-bar class="home">
+      <template v-slot:center>购物街</template>
+    </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import {getHomeMultidata} from 'network/home.js';
+
+import HomeSwiper from "./childComps/HomeSwiper.vue"
+
   export default {
-    name: "Home"
+    name: "Home",
+    data () {
+      return {
+        banner: [],
+        recommend: []
+      }
+    },
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banner = res.data.data.banner.list;
+        this.recommend = res.data.data.recommend.list;
+      })
+    },
   }
 </script>
 
 <style scoped>
-
+.home{
+  background-color: var(--color-tint);
+  color: aliceblue;
+};
 </style>
